358f5aa1c6edeec349ca4ea01838f4563201cf16
alexis lafrance <alexislafrances-laf@outlook.fr>
2020-07-21 16:51:55 +0200

    add theme (not finished)

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../css/app.scss';
+import $ from 'jquery';
+require('bootstrap');
+import '../js/theme/app.js';
+import '../js/theme/scrollIt.min.js';
+import '../js/theme/jquery.magnific-popup.min.js';
+import '../js/theme/owl.carousel.min.js';
+import '../js/theme/jquery.stellar.min.js';
+import '../js/theme/animated.headline.js';
+import '../js/theme/jquery.waypoints.min.js';
+import '../js/theme/jquery.counterup.min.js';
+import '../js/theme/isotope.pkgd.min.js';
+import '../js/theme/validator.js';
+import '../js/theme/custom.js';
+
